b7f2a5ea00f7debdb3efd9a7cfb0540c4f960e32
theoramels <theoramels@gmail.com>
2022-04-05 18:52:37 -0600

    Quick Update
    Implemented Retrieval of  Geometry data

--- ImportData.py
+++ ImportData.py
@@ -5,17 +5,18 @@ import numpy as np
 import pandas as pd
 
 DataDict = {}
-ExcelDF = pd.read_excel('260 Brass Data\Copy of Lab 4 Data.xlsx')
+Mat_For_Analysis = '260Brass' #idea for later implementation
+ExcelDF = pd.read_excel('260BrassData\\260BrassMeasurements.xlsx')
 print(ExcelDF)
+
 # Getting the path for the folder containing the files
-file_location = os.path.join('260 Brass Data', '*.lvm')
+
+file_location = os.path.join('260BrassData', '*.lvm')
 filenames = glob.glob(file_location) # Creating a directory ID
 
 for file in filenames: # for each file in the folder
     with open(file, 'rt') as myfile: #Open
         ind = 0
-        print('The name of file is',file)
-        print('myfile is ',myfile)
         for myline in myfile: # read line by line
             ind += 1
             if '***End_of_Header***' in myline: # Determine where the end of then header is
@@ -26,17 +27,21 @@ for file in filenames: # for each file in the folder
     colNames = ['Time_s', 'Ext_Disp_mm', 'Force_N', 'Globe_Disp_1_mm', 'Globe_Disp_2_mm']
     df.columns = colNames
 
-    # Examples 
-        #dataframes I found to be useful for storing and doing simple algegra on
-    TimeCol = df['Time_s'] # get the values for "column" labeled time in the dataframe "df"
-    df['NEW_DATAFRAME'] = df['Time_s'].add(5) # adds a col named NEW_DATAFRAME w/ values of Time_s + 5
-    DeltaForce = df['Force_N'].diff() #basically the change in force between the current and previous data points
+    # Getting More information from the File Name
+    fileid = os.path.basename(myfile.name)
+    f_name = fileid.replace('.lvm','')
+    material, HeatTreatment, TestNum = f_name.split('_')
+    #print('mat: ' ,material, 'HeatT: ', HeatTreatment, 'Test#: ', TestNum)
 
-        # Numpy arrays are great for a more matlab esk experience
-    Timearray = df['Time_s'].to_numpy() # converts Dataframe to numpy array. (** = ^)
-    df['Time_s'] = Timearray.tolist() # if stuff were done to the numpy array this would replace the previous col with the new values 
-
-    
+    #find the row in Excel DF where Heat treatment and test match
+    GeometryData = ExcelDF[(ExcelDF['HT'] == HeatTreatment) & (ExcelDF['Test_Num'] == TestNum)]
+    #length, width, thickness = GeometryData['Length_mm', 'Width_mm', 'Thickness_mm']
+    length = GeometryData.iloc[0]['Length_mm']
+    width = GeometryData.iloc[0]['Width_mm']
+    thickness = GeometryData.iloc[0]['Thickness_mm']
+    print(length)
+    print(width)
+    print(thickness)
     #Unit Conversion
     #Stress
     #Strain
@@ -44,10 +49,10 @@ for file in filenames: # for each file in the folder
     #True Strain
 
 
-    print(df)
+    
 
-    name = file.replace('.lvm','')
-    DataDict[name] = df
+   
+    DataDict[f_name] = df
 
 for key in DataDict:
     df = DataDict[key]
